e53653f78447ffdd62f6ad83d41e623ef9289bdb
Alasdare Fuller <afullerca@gmail.com>
2019-06-27 18:04:33 -0700

    add style sheet and run.py

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 import sqlite3 #sqlite database
 from functools import wraps 
 
+from flask import Flask, flash, redirect, render_template, \
+request, session, url_for
+
 # config
 
 app = Flask(__name__)
@@ -31,13 +34,13 @@ def logout():
 
 @app.route('/', methods = ['GET', 'POST'])
 def login():
-    if request.method = 'POST':
+    if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] \
                 or request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
             return render_template('login.html', error = error)
         else:
-            session = ['logged_in'] = True
+            session['logged_in'] = True
             flash('Welcome!')
             return redirect(url_for('tasks'))
     return render_template('login.html')    
