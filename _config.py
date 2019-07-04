7c694c7dfaf43fd2daf2113a28be33e5b48c0016
Alasdare Fuller <afullerca@gmail.com>
2019-07-03 19:17:11 -0700

    add SQLalchemy

--- _config.py
+++ _config.py
@@ -6,11 +6,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = "flasktaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
-WTF_CSRF_ENABLED = True #used for cross-site request forgery prevention
-SECRET_KEY = "us6'rhmM+fTrWB8p" #used in conjunction with WTF_CSRF_ENABLED
+CSRF_ENABLED = True #used for cross-site request forgery prevention
+SECRET_KEY = "us6rhmM+fTrWB8p" #used in conjunction with WTF_CSRF_ENABLED
                                 #to create a cryptographic token 
                                 #to validate a form 
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 #define the full path of the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 
--- db_create.py
+++ db_create.py
@@ -1,25 +1,15 @@
-import sqlite3 #sqlite database library
-from _config import DATABASE_PATH #get database location from config file
-    
-with sqlite3.connect(DATABASE_PATH) as connection:
 
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
+from views import db
+from models import Task
+from datetime import date
 
-    # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-        status INTEGER NOT NULL)""")
+# create the database and the db table
+db.create_all()
 
-    #insert dummy data into text
-    c.execute(
-        'INSERT INTO tasks(name, due_date, priority, status)'
-        'VALUES("FINISH THIS TUTORIAL", "03/25/2015", 10, 1)'
-    )
+# insert data
+db.session.add(Task("Finish this Tutorial", date(2016, 9, 22), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, 1))
 
-    c.execute(
-        'INSERT INTO tasks(name, due_date, priority, status)'
-        'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-    )
-
+# commit the changes
+db.session.commit()
 
