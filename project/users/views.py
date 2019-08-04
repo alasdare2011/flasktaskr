89a1084780d5ff3464314f8c5fbc98d62e0fed8a
Alasdare Fuller <afullerca@gmail.com>
2019-08-03 19:25:28 -0700

    correct user register password hash

--- project/users/views.py
+++ project/users/views.py
@@ -82,8 +82,8 @@ def register():
             new_user = User(
                 form.name.data,
                 form.email.data,
-                form.password.data
-                #bcrypt.generate_password_hash(form.password.data)
+                #form.password.data
+                bcrypt.generate_password_hash(form.password.data)
             )
             try:
                 db.session.add(new_user)
